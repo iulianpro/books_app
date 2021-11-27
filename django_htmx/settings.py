41b0d4854dac22067ad59c5dec7eea53cf3d34d3
iulianpro <iulian.prodea@gmail.com>
2021-11-27 17:10:27 +0000

    added new app

--- django_htmx/settings.py
+++ django_htmx/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0b*0ciy2cs@-^4rj7kt0csc)yum5_3op5l0bw520%deu7q8%cg'
+SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
